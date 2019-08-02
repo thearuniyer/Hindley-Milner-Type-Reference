da0b72bff317c490f74a04e5763a26cab956a46c
Arun Shankar <ashank16@asu.edu>
2019-08-02 13:22:31 -0700

    parser created

---
+++ parser.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <istream>
+#include <vector>
+#include <string>
+#include <cctype>
+#include <stdlib.h>
+#include <string.h>
+#include <algorithm>
+
+#include "parser.h"
+#include "lexer.h"
+#include "inputbuf.h"
+
+using namespace std;
---
+++ parser.h
@@ -0,0 +1,32 @@
+#ifndef __PARSER__H__
+#define __PARSER__H__
+
+#include <vector>
+#include <string>
+
+#include "inputbuf.h"
+#include "lexer.h"
+
+class Parser{
+  void parse_program();
+  void parse_global_vars();
+  void parse_var_decl_list();
+  void parse_var_decl();
+  void parse_var_list();
+  void parse_type_name();
+  void parse_body();
+  void parse_stmt_list();
+  void parse_stmt();
+  void parse_assign_stmt();
+  void parse_expr();
+  void parse_unary_operator();
+  void parse_binary_operator();
+  void parse_primary();
+  void parse_if_stmt();
+  void parse_while_stmt();
+  void parse_switch_stmt();
+  void parse_case_list();
+  void parse_case();
+};
+
+#endif  //__PARSER__H__
